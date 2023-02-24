655470d4107d62753b2f322035fc423067811f20
housten <14905544+housten@users.noreply.github.com>
2023-02-24 10:42:28 +0100

    Add first script

---
+++ OD_update_libraryset_variable.ps1
@@ -0,0 +1,37 @@
+﻿$ErrorActionPreference = "Stop";
+# Define working variables
+$octopusURL = "https://solidifyheidi.octopus.app"
+$octopusAPIKey = "API-ALSHOJT1EJFG12BVTEKTUXEOX30I3MMC"
+$header = @{ "X-Octopus-ApiKey" = $octopusAPIKey }
+
+# Specify the Space to search in
+$spaceId = "Spaces-1"
+
+# Variable name to search for
+$VariableName = "Variable1"
+
+# New variable value to set
+$VariableValue = "auto"
+
+# Get variable set from Octopus
+# library url
+$LibraryVariableSetVariables = (Invoke-RestMethod -Method Get -Uri "$OctopusURL/api/$spaceId/variables/variableset-LibraryVariableSets-21" -Headers $Header) 
+# project url
+#$LibraryVariableSetVariables = (Invoke-RestMethod -Method Get -Uri "$OctopusURL/api/$spaceId/variables/variableset-Projects-1" -Headers $Header) 
+
+
+# Find the variable
+$index = $LibraryVariableSetVariables.Variables.name.IndexOf($variableName)
+
+ 
+if ($index -ge 0) {
+  # if the variable was found, update it.
+  $LibraryVariableSetVariables.Variables[$index].value = $variableValue
+  $UpdatedLibraryVariableSet = Invoke-RestMethod -Method Put -Uri "$OctopusURL/api/$spaceId/variables/variableset-Projects-1" -Headers $Header -Body ($LibraryVariableSetVariables | ConvertTo-Json -Depth 10)   
+} else {
+  # Otherwise write out an error
+  Write-Output ("Variable name [$variableName] not found. These are the available variable names:[{0}]" -f (($LibraryVariableSetVariables.Variables.name) -join(',')))
+}
+
+# Write out response from update
+write-host ($UpdatedLibraryVariableSet | convertto-json )
